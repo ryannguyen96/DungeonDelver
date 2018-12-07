7e2e04fa9f9d6c62157a8149b90d2b8201f2a7db
Ryan Nguyen <shiga2714@gmail.com>
2018-12-07 04:49:15 -0500

    Added aligning scripts
    - Dray and Skeletos now align with the grid properly
    - Dray can more easily align with doors

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dray : MonoBehaviour
+public class Dray : MonoBehaviour, IFacingMover
 {
     public enum eMode { idle, move, attack, transition }
 
@@ -19,6 +19,7 @@ public class Dray : MonoBehaviour
     private float timeAtkNext = 0;
     private Rigidbody rigid;
     private Animator anim;
+    private InRoom inRm;                                       
 
     private Vector3[] directions = new Vector3[] {
         Vector3.right, Vector3.up, Vector3.left, Vector3.down
@@ -30,6 +31,7 @@ public class Dray : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        inRm = GetComponent<InRoom>();
     }
     void Update()
     {
@@ -85,5 +87,62 @@ public class Dray : MonoBehaviour
                 break;
         }
         rigid.velocity = vel * speed;
+    }
+    
+    // Implementation of IFacingMover
+    public int GetFacing()
+    {
+        return facing;
+    }
+
+    public bool moving
+    {
+        get
+        {
+            return(mode == eMode.move);
+        }
+    }
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    public float gridMult
+    {
+        get
+        {
+            return inRm.gridMult;
+        }
+    }
+
+    public Vector2 roomPos
+    {
+        get
+        {
+            return inRm.roomPos;
+        }
+
+        set
+        {
+            inRm.roomPos = value;
+        }
+    }
+
+    public Vector2 roomNum
+    {
+        get
+        {
+            return inRm.roomNum;
+        }
+        set
+        {
+            inRm.roomNum = value;
+        }
+    }
+
+    public Vector2 GetRoomPosOnGrid(float mult = -1)
+    {
+        return inRm.GetRoomPosOnGrid(mult);
     }
 }
--- Assets/__Scripts/Skeletos.cs
+++ Assets/__Scripts/Skeletos.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeletos : Enemy
+public class Skeletos : Enemy, IFacingMover
 {
     [Header("Set in Inspector: Skeletos")]
     public int speed = 2;
@@ -12,6 +12,13 @@ public class Skeletos : Enemy
     [Header("Set Dynamically: Skeletos")]
     public int facing = 0;
     public float timeNextDecision = 0;
+    private InRoom inRm;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        inRm = GetComponent<InRoom>();
+    }
 
     void Update()
     {
@@ -27,5 +34,53 @@ public class Skeletos : Enemy
     {
         facing = Random.Range(0, 4);
         timeNextDecision = Time.time + Random.Range(timeThinkMin, timeThinkMax);
+    }
+
+    // Implementation of IFacingMover
+    public int GetFacing()
+    {
+        return facing;
+    }
+
+    public bool moving
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    public float GetSpeed()
+    {
+        return speed;
+    }
+
+    public float gridMult
+    {
+        get { return inRm.gridMult; }
+    }
+
+    public Vector2 roomPos
+    {
+        get
+        {
+            return inRm.roomPos;
+        }
+        set { inRm.roomPos = value; }
+    }
+
+    public Vector2 roomNum
+    {
+        get
+        {
+            return inRm.roomNum;
+        }
+        set { inRm.roomNum = value; }
+    }
+
+    public
+    Vector2 GetRoomPosOnGrid(float mult = -1)
+    {
+        return inRm.GetRoomPosOnGrid(mult);
     }
 }
