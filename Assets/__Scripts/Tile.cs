8d24f1d8b258d3a7c36cdad7f840500b992e3058
Ryan Nguyen <shiga2714@gmail.com>
2018-12-07 04:22:12 -0500

    Added per-tile collision script
    - More refined script for collisions
    - Dray is now unable to walk past walls, but he is able to enter doorways

--- Assets/__Scripts/Tile.cs
+++ Assets/__Scripts/Tile.cs
@@ -7,6 +7,13 @@ public class Tile : MonoBehaviour
     public int x;
     public int y;
     public int tileNum;
+    private BoxCollider bColl;
+
+    void Awake()
+    {
+        bColl = GetComponent<BoxCollider>();
+    }
+
     public void SetTile(int eX, int eY, int eTileNum = -1)
     {
         x = eX;
@@ -19,5 +26,63 @@ public class Tile : MonoBehaviour
         }
         tileNum = eTileNum;
         GetComponent<SpriteRenderer>().sprite = TileCamera.SPRITES[tileNum];
+        SetCollider();
     }
+
+    // Arrange the collider for this tile
+
+    void
+
+    SetCollider()
+    {
+        // Collider info is pulled from DelverCollisions.txt
+        bColl.enabled = true;
+        char c = TileCamera.COLLISIONS[tileNum];
+        switch(c)
+        {
+            case 'S': // Whole
+                bColl.center = Vector3.zero;
+                bColl.size = Vector3.one;
+                break;
+            case 'W': // Top
+                bColl.center = new Vector3(0, 0.25f, 0);
+                bColl.size = new Vector3(1, 0.5f, 1);
+                break;
+            case 'A': // Left
+                bColl.center = new Vector3(-0.25f, 0, 0);
+                bColl.size = new Vector3(0.5f, 1, 1);
+                break;
+            case 'D': // Right
+                bColl.center = new Vector3(0.25f, 0, 0);
+                bColl.size = new Vector3(0.5f, 1, 1);
+                break;
+            // vvvvvvvv-------- These are optional --------vvvvvvvv          
+            case 'Q': // Top, Left
+                bColl.center = new Vector3(-0.25f, 0.25f, 0);
+                bColl.size = new Vector3(0.5f, 0.5f, 1);
+                break;
+            case 'E': // Top, Right
+                bColl.center = new Vector3(0.25f, 0.25f, 0);
+                bColl.size = new Vector3(0.5f, 0.5f, 1);
+                break;
+            case 'Z': // Bottom, left
+                bColl.center = new Vector3(-0.25f, -0.25f, 0);
+                bColl.size = new Vector3(0.5f, 0.5f, 1);
+                break;
+            case 'X': // Bottom
+                bColl.center = new Vector3(0, -0.25f, 0);
+                bColl.size = new Vector3(1, 0.5f, 1);
+                break;
+            case 'C': // Bottom, Right
+                bColl.center = new Vector3(0.25f, -0.25f, 0);
+                bColl.size = new
+                Vector3(0.5f, 0.5f, 1);
+                break;
+            // ^^^^^^^^-------- These are optional --------^^^^^^^^          
+
+            default: // Anything else: _, |, etc.
+                bColl.enabled = false;
+                break;
+        }
+    }
 }
--- Assets/__Scripts/TileCamera.cs
+++ Assets/__Scripts/TileCamera.cs
@@ -9,6 +9,7 @@ public class TileCamera : MonoBehaviour
     static public Sprite[] SPRITES;
     static public Transform TILE_ANCHOR;
     static public Tile[,] TILES;
+    static public string COLLISIONS;
     [Header("Set in Inspector")]
     public TextAsset mapData;
     public Texture2D mapTiles;
@@ -17,6 +18,7 @@ public class TileCamera : MonoBehaviour
 
     void Awake()
     {
+        COLLISIONS = Utils.RemoveLineEndings(mapCollisions.text);
         LoadMap();
     }
 
