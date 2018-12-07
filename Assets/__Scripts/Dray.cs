30cf933af42ff4f19c1252c37cf764dafd4d3dd3
Ryan Nguyen <shiga2714@gmail.com>
2018-12-07 17:37:13 -0500

    Grappler fully implemented and working
    - Added Grappler script
    - Grappler can now deal damage, grab items, or pull Dray towards walls

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -22,6 +22,9 @@ public class Dray : MonoBehaviour, IFacingMover, IKeyMaster
     public eMode mode = eMode.idle;
     public int numKeys = 0;
     public bool invincible = false;
+    public bool hasGrappler = false;
+    public Vector3 lastSafeLoc;
+    public int lastSafeFacing;
 
     [SerializeField]
     private int _health;
@@ -59,6 +62,9 @@ public class Dray : MonoBehaviour, IFacingMover, IKeyMaster
         anim = GetComponent<Animator>();
         inRm = GetComponent<InRoom>();
         health = maxHealth;
+        lastSafeLoc = transform.position;
+        // The start position is safe.
+        lastSafeFacing = facing;
     }
     void Update()
     {
@@ -179,6 +185,8 @@ public class Dray : MonoBehaviour, IFacingMover, IKeyMaster
                 roomNum = rm;
                 transitionPos = InRoom.DOORS[(doorNum + 2) % 4];
                 roomPos = transitionPos;
+                lastSafeLoc = transform.position;
+                lastSafeFacing = facing;
                 mode = eMode.transition;
                 transitionDone = Time.time + transitionDelay;
             }
@@ -238,8 +246,20 @@ public class Dray : MonoBehaviour, IFacingMover, IKeyMaster
             case PickUp.eType.key:
                 keyCount++;
                 break;
+            case PickUp.eType.grappler:
+                hasGrappler = true;
+                break;
         }
         Destroy(colld.gameObject);
+    }
+
+    public void ResetInRoom(int healthLoss = 0)
+    {
+        transform.position = lastSafeLoc;
+        facing = lastSafeFacing;
+        health -= healthLoss;
+        invincible = true; // Make Dray invincible
+        invincibleDone = Time.time + invincibleDuration;
     }
 
 
